f37e02c82bde7684505c924e9ff9ccd803643651
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2017-11-16 20:22:09 +0700

    check nim validation

--- app/src/main/java/sttnf/app/pemira/core/overview/OverviewActivity.java
+++ app/src/main/java/sttnf/app/pemira/core/overview/OverviewActivity.java
@@ -2,7 +2,14 @@ package sttnf.app.pemira.core.overview;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import sttnf.app.pemira.R;
 import sttnf.app.pemira.base.BaseActivity;
 
@@ -13,6 +20,10 @@ import sttnf.app.pemira.base.BaseActivity;
 
 public class OverviewActivity extends BaseActivity<OverviewPresenter> implements OverviewView {
 
+    @BindView(R.id.layout_caution) LinearLayout layoutCaution;
+    @BindView(R.id.txt_caution) TextView txtCaution;
+    @BindView(R.id.edt_nim) EditText edtNim;
+
     @Override protected OverviewPresenter initPresenter() {
         return new OverviewPresenter(this);
     }
@@ -20,6 +31,28 @@ public class OverviewActivity extends BaseActivity<OverviewPresenter> implements
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding(R.layout.activity_overview);
+        checkNim();
+    }
+
+    private void checkNim() {
+        edtNim.addTextChangedListener(new TextWatcher() {
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (s.length() == 0) layoutCaution.setVisibility(View.GONE);
+            }
+            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (count < 10) {
+                    layoutCaution.setVisibility(View.VISIBLE);
+                    txtCaution.setText("NIM anda tidak terdaftar");
+                } else {
+                    layoutCaution.setVisibility(View.GONE);
+                }
+            }
+            @Override public void afterTextChanged(Editable s) {}
+        });
+    }
+
+    private void isValidNim(String nim) {
+        
     }
 
 }
--- app/src/main/java/sttnf/app/pemira/network/Network.java
+++ app/src/main/java/sttnf/app/pemira/network/Network.java
@@ -6,6 +6,7 @@ import com.google.gson.GsonBuilder;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import sttnf.app.pemira.BuildConfig;
 
 /**
  * Created by isfaaghyth on 11/16/17.
@@ -16,7 +17,7 @@ public class Network {
     public static Retrofit CLIENT() {
         Gson gson = new GsonBuilder().setLenient().create();
         return new Retrofit.Builder()
-                .baseUrl("")
+                .baseUrl(BuildConfig.MAIN_URL)
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
